97d3559fa44b93ff0858f869eff0f4439981b3b0
binghan60 <106055228+binghan60@users.noreply.github.com>
2025-03-18 08:47:35 +0000

    de

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+import * as line from '@line/bot-sdk'
+import express from 'express'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import User from './models/userSchema.js';
+import Todo from './models/todoSchema.js';
+
+dotenv.config();
+const config = {
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+const client = new line.messagingApi.MessagingApiClient({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
+});
+const app = express();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('資料庫連線成功');
+  })
+  .catch((err) => {
+    console.log('資料庫連線失敗');
+    console.err(err.message);
+  });
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
 async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return client.replyMessage({
@@ -96,3 +131,12 @@ async function handleEvent(event) {
     messages: [flexMessage],
   });
 }
+
+
+
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
