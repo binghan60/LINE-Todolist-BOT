aa492311d69ce135209036312bfe1cf2bdd3bd93
binghan60 <106055228+binghan60@users.noreply.github.com>
2025-03-18 09:55:36 +0000

    de

--- index.js
+++ index.js
@@ -105,7 +105,8 @@ async function handleEvent(event) {
                   text: `delete:${index}`  // 點擊刪除按鈕時發送 `delete:index` 訊息
                 },
                 flex: 1,
-                height: "sm"
+                height: "sm",
+                align: "center"
               }
             ]
           }))
